c0aa7c23217f6992e48f5a218b0ddb8164699f32
SivaNarayana <e.siva306@gmail.com>
2025-07-10 13:50:22 +0530

    Add Spring Cloud Function Experience with Spring Cloud Function

---
+++ producer/src/main/java/com/example/producer/Function/FunctionClass.java
@@ -0,0 +1,14 @@
+package com.example.producer.Function;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Function;
+
+@Configuration
+public class FunctionClass {
+    @Bean
+    public Function<String, String> upperCase(){
+        return String::toUpperCase;
+    }
+}
