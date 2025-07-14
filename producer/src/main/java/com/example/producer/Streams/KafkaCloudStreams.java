1251ccb818c9df6c814624213f67f9e88dcf77d8
SivaNarayana <e.siva306@gmail.com>
2025-07-14 14:41:50 +0530

    working with partitions

--- producer/src/main/java/com/example/producer/Streams/KafkaCloudStreams.java
+++ producer/src/main/java/com/example/producer/Streams/KafkaCloudStreams.java
@@ -4,13 +4,15 @@ import com.example.producer.Model.Rider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Random;
 import java.util.function.Supplier;
 @Configuration
 public class KafkaCloudStreams {
     @Bean
     public Supplier<Rider> sendRider() {
+        Random random = new Random();
         return () -> {
-            Rider rider = new Rider("John", "Doe", 30);
+            Rider rider = new Rider("RiderId-" + random.nextInt(20), "Doe", 30);
             System.out.println("Message sent to Kafka topic: " + rider.getFirstName());
             return rider;
         };
